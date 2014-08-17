73ecb8bb1b8750b58a5e9fdf5802f694e72f7bda
Tommy Nevtelen <tommy@nevtelen.com>
2014-08-17 20:39:26 +0200

    Linux midi port names for auto detection

--- SLMkII.control.js
+++ SLMkII.control.js
@@ -16,7 +16,16 @@ var controller = null;
 
 host.defineController ("Novation", "SLMkII", "1.0", "D1CEE920-1E51-11E4-8C21-0800200C9A66");
 host.defineMidiPorts (2, 1);
-host.addDeviceNameBasedDiscoveryPair (["MIDIIN2 (SL MkII)", "SL MkII"], ["MIDIOUT2 (SL MkII)"]);
+
+if(host.platformIsLinux())
+{
+	host.addDeviceNameBasedDiscoveryPair (["SL MkII MIDI 2", "SL MkII MIDI 1"], ["SL MkII MIDI 2"]);
+}
+
+if(host.platformIsWindows())
+{
+	host.addDeviceNameBasedDiscoveryPair (["MIDIIN2 (SL MkII)", "SL MkII"], ["MIDIOUT2 (SL MkII)"]);
+}
 
 function init ()
 {
